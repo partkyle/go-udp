8749ba2e3626d3f001589e3db8222854792b228f
Kyle Partridge <partkyle@gmail.com>
2014-07-11 11:11:19 -0700

    change source address to all interfaces; if payload is not passed in use stdin

--- client/client.go
+++ client/client.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 var (
-	host = flag.String("host", "127.0.0.1", "host to send udp to")
+	logger = log.New(os.Stdout, "[udp-server] ", log.LstdFlags)
+
+	host = flag.String("host", "0.0.0.0", "host to send udp to")
 	port = flag.Int("port", 0, "port to send udp to")
 
 	payload = flag.String("payload", "", "data to send over udp")
@@ -19,16 +23,27 @@ func main() {
 
 	ip := net.ParseIP(*host)
 
-	srcAddr := &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 0}
+	srcAddr := &net.UDPAddr{IP: []byte{0, 0, 0, 0}, Port: 0}
 	dstAddr := &net.UDPAddr{IP: ip, Port: *port}
 
-	log.Printf("sending from %s to %s", srcAddr, dstAddr)
+	logger.Printf("sending from %s to %s", srcAddr, dstAddr)
 
 	conn, err := net.DialUDP("udp", srcAddr, dstAddr)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer conn.Close()
 
-	fmt.Fprintf(conn, "%s", *payload)
+	var r io.Reader = strings.NewReader(*payload)
+	if *payload == "" {
+		logger.Printf("using stdin for payload")
+		r = os.Stdin
+	}
+
+	n, err := io.Copy(conn, r)
+	if err != nil {
+		logger.Fatal("error sending data: %s", err)
+	}
+
+	logger.Printf("sent %d bytes", n)
 }
